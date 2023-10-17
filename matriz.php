a58ab86dabd54fc134fa9c282652c36889cd166f
Emerson <emer00k@gmail.com>
2023-10-17 11:57:11 -0300

    Limpeza de comentarios

--- matriz.php
+++ matriz.php
@@ -15,20 +15,16 @@ class MatrizCusto {
        
         $result = pg_query($oConexao, $sSelect);
 
-        // Verifica se há resultados
         if ($result) {
-            // Inicializa uma matriz para armazenar os dados
             $matriz = array();
     
-            // Itera pelos resultados e adiciona os dados à matriz
             while ($row = pg_fetch_assoc($result)) {
                 $matriz[] = $row;
             }
-    
-            // Fecha o resultado
+            
             pg_free_result($result);
     
-            // Agora, $matriz contém os dados do banco de dados na forma de uma matriz associativa
+
             $this->setMatriz($matriz);
     
         } else {
